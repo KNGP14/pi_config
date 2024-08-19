875a83b26683619bbef2440951312faee279dd56
Kevin Neumann <KNGP14@users.noreply.github.com>
2024-08-19 14:04:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ echo "The script you are running has:"
 echo "basename: [$(basename "$0")]"
 echo "dirname : [$(dirname "$0")]"
 echo "pwd     : [$(pwd)]"
+echo ""
+echo "mkdir $(dirname "$0")/venv"
 
 #mkdir venv
 #python3 -m venv venv/
