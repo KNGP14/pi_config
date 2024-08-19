04be3fefe994b7e7a89e2c6d0d31324ebfc7da06
Kevin Neumann <KNGP14@users.noreply.github.com>
2024-08-19 14:00:47 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "The script you are running has:"
+echo "basename: [$(basename "$0")]"
+echo "dirname : [$(dirname "$0")]"
+echo "pwd     : [$(pwd)]"
+
+#mkdir venv
+#python3 -m venv venv/
