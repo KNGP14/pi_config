301401dace5c5cb020943655ed1f90f235391128
Kevin Neumann <KNGP14@users.noreply.github.com>
2024-08-19 14:58:56 +0200

    Update pi_config.sh

--- pi_config.sh
+++ pi_config.sh
@@ -7,4 +7,11 @@ do
     args="$args $arg";
     i=$((i + 1));
 done
-venv/bin/python3 pi_config.py $args
+
+DIR=`dirname "$0"`
+python3="$DIR/venv/bin/python3"
+if [ ! -f "$python3" ]; then
+    python3="$DIR/venv/Scripts/python.exe"
+fi
+
+$python3 pi_config.py $args
